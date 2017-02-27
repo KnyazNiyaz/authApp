4776d5d2364198e0021d9c1c6196c134dd3f0f15
BTorokulov <!Auca917382465>
2017-02-28 02:12:42 +0600

    Added Auth Guard  n protected requests

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Http, Headers} from '@angular/http';
 import 'rxjs/add/operator/map';
+import { tokenNotExpired } from 'angular2-jwt'
 
 @Injectable()
 export class AuthService {
@@ -21,12 +22,28 @@ export class AuthService {
     return this.http.post('http://localhost:3000/users/authenticate', user, {headers: headers}).map(res => res.json())
   }
 
+  getProfile(){
+    let headers = new Headers();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    return this.http.get('http://localhost:3000/users/profile', {headers: headers}).map(res => res.json())
+  }
+
+  loadToken(){
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  }
+
   storeUserData(token, user) {
     localStorage.setItem('id_token', token);
     localStorage.setItem('user', JSON.stringify(user));
     console.log(localStorage);
     this.authToken = token;
     this.user = user;
+  }
+
+  loggedIn(){
+    return tokenNotExpired();
   }
 
   logout(){
